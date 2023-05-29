b0030b36c6b95cf769156610677258f172d7d8b5
Tatiana Barrera <barreratatiana709@gmail.com>
2023-05-29 14:19:15 -0500

    formularios usuario terminados

--- src/components/EmailRecoverPassword.tsx
+++ src/components/EmailRecoverPassword.tsx
@@ -5,28 +5,26 @@ export const EmailRecoverPassword = () => {
     return (
      
    
-        <div className="w-[500px] shadow-xl border rounded-[10px]">
-          <div className="mx-2 my-20 sm:my-auto  w-">
+        <div className="w-full md:w-[500px] shadow-xl border rounded-[10px]">
+          <div className="mx-2 md:my-0">
              
               <div className="
-                  px-6 py-10 sm:px-10 sm:py-6 
-                  bg-white rounded-lg shadow-md lg:shadow-lg">
+                  px-6 py-10 ">
       
                  
-                  <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-800 mb-1 ">
+                  <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-800 ">
                       Recuperar contraseña
                   </h2>
       
                      <p className="text-sm text-left mt-3">Ingrese la dirección de correo electrónico verificada de su cuenta de usuario y le enviaremos un enlace para restablecer la contraseña.</p>
                     
-                  <form className="mt-1" method="POST">
+                  <form className="mt-3" method="POST">
                      
 
                   <div className="flex items-center justify-center border-2 rounded-[10px] my-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M12 1.95c-5.52 0-10 4.48-10 10s4.48 10 10 10h5v-2h-5c-4.34 0-8-3.66-8-8s3.66-8 8-8s8 3.66 8 8v1.43c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57v-1.43c0-2.76-2.24-5-5-5s-5 2.24-5 5s2.24 5 5 5c1.38 0 2.64-.56 3.54-1.47c.65.89 1.77 1.47 2.96 1.47c1.97 0 3.5-1.6 3.5-3.57v-1.43c0-5.52-4.48-10-10-10zm0 13c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3z"/></svg>
+                <span className="px-1 block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M12 1.95c-5.52 0-10 4.48-10 10s4.48 10 10 10h5v-2h-5c-4.34 0-8-3.66-8-8s3.66-8 8-8s8 3.66 8 8v1.43c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57v-1.43c0-2.76-2.24-5-5-5s-5 2.24-5 5s2.24 5 5 5c1.38 0 2.64-.56 3.54-1.47c.65.89 1.77 1.47 2.96 1.47c1.97 0 3.5-1.6 3.5-3.57v-1.43c0-5.52-4.48-10-10-10zm0 13c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3z"/></svg></span>
                       <input id="email" type="email" name="email" placeholder="Email" 
-                          className="block w-full p-2
-                          "
+                          className="block w-full py-4 outline-none text-xl"
                           required />
                 </div>
       
--- src/components/RestorePassword.tsx
+++ src/components/RestorePassword.tsx
@@ -1,13 +1,12 @@
 export const RestorePassword = () => {
   return (
 
-    <div className="w-[500px] shadow-xl border rounded-[10px]">
+    <div className="w-full md:w-[500px] shadow-xl border rounded-[10px]">
     
-    <div className="mx-2 my-20 sm:my-auto  w-">
+    <div className="mx-2 md:my-0">
              
               <div className="
-                  px-6 py-10 sm:px-10 sm:py-6 
-                  bg-white rounded-lg shadow-md lg:shadow-lg">
+                  px-6 py-10 ">
       
                  
                   <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-800  ">
@@ -18,15 +17,17 @@ export const RestorePassword = () => {
                      
                     
                   <div className="flex items-center justify-center border-2 rounded-[10px] my-3">
+                  <span className="px-1 block">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3Z"/></svg>
+                  </span>
                       <input id="password" type="password" name="password" placeholder="Contraseña" 
-                          className="block w-full p-2"
+                          className="block w-full py-4 outline-none text-xl"
                           required />
                   </div>
                   <div className="flex items-center justify-center border-2 rounded-[10px] my-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3Z"/></svg>
+                 <span className="px-1 block"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3Z"/></svg></span>
                       <input id="password" type="password" name="password" placeholder="Confirmar contraseña" 
-                          className="block w-full py-2"
+                          className="block w-full py-4 outline-none text-xl"
                           required />
                   </div>
                       
