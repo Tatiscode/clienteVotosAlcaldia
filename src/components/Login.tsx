16209406b6f4d0c19f29f3ddd1e2bc05e6696ea0
Tatiana Barrera <barreratatiana709@gmail.com>
2023-05-29 13:42:50 -0500

    login terminado 3

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -25,7 +25,7 @@ export const Login = () => {
                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="gray" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/></svg>
                </span>
                      <input id="user" type="text" name="user" placeholder="Usuario" 
-                         className="block w-full py-4 outline-none "
+                         className="block w-full py-4 outline-none text-xl"
                          required />
                </div>
      
@@ -34,7 +34,7 @@ export const Login = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="gray" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3Z"/></svg>
                 </span>
                      <input id="password" type="password" name="password" placeholder="Contraseña" 
-                         className="block w-full py-4 outline-none"
+                         className="block w-full py-4 outline-none text-xl "
                          required />
                  </div>
                      
