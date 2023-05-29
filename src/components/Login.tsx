7c0badfd783e50a9d6a7e7f34dcd3425b35285aa
Tatiana Barrera <barreratatiana709@gmail.com>
2023-05-29 14:00:42 -0500

    registro  terminado 4

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,13 +7,10 @@ export const Login = () => {
    
           
         <div className=" w-full md:w-[500px] shadow-xl border rounded-[10px]"> 
-              <div className="mx-2 md:my-0  sm:my-auto     ">
+              <div className="mx-2 md:my-0  ">
              
-             <div className="
-                 px-6 py-10  
-                  ">
+             <div className="px-6 py-10">
      
-                
                  <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-800">
                      Iniciar sesion
                  </h2>
